971780da7094dce6dbd0b8d54088eac4e99f9cd1
plunkydev <plunkydesign@gmail.com>
2025-09-10 08:49:08 -0500

    feat: add Node and LinkedList structures and initialize each HashMap bucket with a linked list

--- hash-map.js
+++ hash-map.js
@@ -1,13 +1,37 @@
+class Node {
+    constructor(key, value, next = null) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+    insert(key, value) {
+        const newNode = new Node(key, value);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.size++;
+    }
+}
 class HashMap {
     constructor(capacity, loadFactor) {
         this.capacity = capacity;
         this.loadFactor = loadFactor;
         this.buckets = new Array(capacity);
         this.size = 0;
+
+        // Initialize the buckets with LinkedList instances
+        for (let i = 0; i < this.buckets.length; i++) {
+            this.buckets[i] = new LinkedList();
+        }
     }
-    
 }
 
 const map = new HashMap(16, 0.75)
 
-console.log(map.buckets.length)
+console.log(map.buckets)
