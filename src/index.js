a6fa8a11c46d80b842b4e8bc585e60899eba2f3a
plunkydev <plunkydesign@gmail.com>
2025-09-12 18:00:41 -0500

    feat: integrar renderNode y función displayBucket para mostrar buckets de HashMap en el DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import { HashMap } from '/src/hash-map.js';
+import { renderNode } from './node'
 import './style.css'
 const test = new HashMap()
+const main = document.getElementById("data_hashMap")
+
+const displayBucket = (index) => {
+    const bucketDiv = document.getElementById(`node${index}`)
+    bucketDiv.innerText = ''
+    if (test.buckets[index].head === null) {
+        bucketDiv.innerText = "null"
+    } else {
+        let current = test.buckets[index].head
+        while(current) {
+            bucketDiv.innerText += `  [${current.key} => ${current.value}] __`
+            current = current.next
+        }
+    }
+    return true
+}
+
+
+
 
 //pruebas para la funcion set
 test.set("apple", "red")
@@ -15,7 +35,7 @@ test.set('ice cream', 'white')
 test.set('jacket', 'blue')
 test.set('kite', 'pink')
 //pruebas para la funcion resize
-test.set('lion', 'golden')
+//test.set('lion', 'golden')
 /* test.set('monkey', 'tan')
 test.set('notebook', 'silver')
 test.set('orange', 'orange')
@@ -23,10 +43,18 @@ test.set('pencil', 'yellow')
 test.set('queen', 'purple')
 test.set('robot', 'metallic')
 test.set('sun', 'yellow') */
-console.log(test.remove('dog'))
+//console.log(test.remove('dog'))
 //test.set('tiger', 'orange')
 //console.log(test.remove('lion'))
 //console.log(test.clear())
 //console.log(test.keys())
-console.log(test.values())
-console.log(test.buckets)
+//console.log(test.values())
+//console.log(displayBucket(9))
+console.log(test.capacity)
+for (let i = 0; i < test.capacity; i++) {
+    main.appendChild(renderNode(i))
+}
+//iterar solo para prueba de la funcion displayBucket
+for (let i = 0; i < test.capacity; i++) {
+    displayBucket(i)
+}
